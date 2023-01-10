0b1279ce3cb32f445a9225e70af1d0678bc2caf4
Gabriel Milhomem <gabriel.milhomem@ul.com>
2023-01-09 23:04:05 -0300

    (Object-Oriented Design) Finalizando atividade final

---
+++ Object-Oriented Design/SharingApp/app/src/main/java/com/example/sharingapp/Contact.java
@@ -0,0 +1,57 @@
+package com.example.sharingapp;
+
+import java.util.UUID;
+
+public class Contact {
+
+    private String username;
+    private String email;
+    private String id;
+
+    public Contact(String username, String email, String id) {
+        this.username = username;
+        this.email = email;
+        if (id == null){
+            setId();
+        } else {
+            updateId(id);
+        }
+    }
+
+    public void setId() {
+        this.id = UUID.randomUUID().toString();
+    }
+
+    public String getId(){
+        return this.id;
+    }
+
+    public void updateId(String id){
+        this.id = id;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    @Override
+    public String toString() {
+        return "Contact{" +
+                "username='" + username + '\'' +
+                ", email='" + email + '\'' +
+                ", id='" + id + '\'' +
+                '}';
+    }
+}
