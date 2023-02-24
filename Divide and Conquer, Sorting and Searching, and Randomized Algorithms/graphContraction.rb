e9361abec4b36091d04367bd194d3c61a21336ad
Gabriel Milhomem <gabriel.milhomem@ul.com>
2023-02-24 19:15:40 -0300

    (Algorithms 1) Continuando implementação do graph contraction

--- Divide and Conquer, Sorting and Searching, and Randomized Algorithms/graphContraction.rb
+++ Divide and Conquer, Sorting and Searching, and Randomized Algorithms/graphContraction.rb
@@ -1,5 +1,3 @@
-require 'set'
-
 class Graph
     attr_accessor :nodes
 
@@ -9,6 +7,36 @@ class Graph
 
     def to_s
         @nodes.map{|n| n.to_s.gsub! '"', ''}.to_s
+    end
+
+    def contract(edge)
+        
+        #contracting edge
+        u, v = edge
+        uv_value = u.value.to_s + "-" + v.value.to_s
+        uv_neighbours = u.neighbors | v.neighbors
+        uv_neighbours = uv_neighbours - edge
+
+        uv = Node.new(uv_value, uv_neighbours)
+
+        # removing u and v from other nodes and adding uv in their place
+        self.nodes = self.nodes - edge
+        self.nodes.push(uv)
+
+        self.nodes.each do |node|
+            if node.neighbors.member?(u) || node.neighbors.member?(v)
+                node.neighbors.delete(u)
+                node.neighbors.delete(v)
+                node.neighbors.push(uv)
+            end
+        end
+        self
+    end
+
+    def get_random_edge()
+        u = self.nodes.sample
+        v = u.neighbors.sample
+        [u, v]
     end
 end
 
@@ -22,47 +50,56 @@ class Node
     def to_s
         "value: " + @value.to_s + " - neighbors: "+ @neighbors.map{|v| v.value.to_s}.to_s
     end
+
+    def ==(other)
+        if !(other.instance_of? Node)
+            return false
+        end
+        self.value == other.value && self.neighbors == other.neighbors
+    end
+
+    def hash
+        [self.value, self.neighbors].hash
+    end
 end
 
 def graph_contraction(graph)
     if graph.nodes.length == 2
-        return graph.nodes
+        return graph
     end
 
-    edge = choose_edge(graph)
-
-    contracted_graph = contract_graph(graph, edge)
-
-    graph_contraction(contracted_graph)
-
+    graph.contract(graph.get_random_edge)
+    graph_contraction(graph)
 end
 
-def choose_edge(graph)
-    u = graph.nodes.to_a.sample
-    v = u.neighbors.to_a.sample
-    [u, v]
+def get_graph
+    a = Node.new("a", [])
+    b = Node.new("b", [])
+    c = Node.new("c", [a, b])
+
+    b.neighbors = [a, c]
+    a.neighbors = [b, c]
+
+    d = Node.new("d", [c])
+    Graph.new([a, b, c, d])
 end
 
-def contract_graph(graph, edge)
-    [u, v] = edge
-    uv = Node.new()
-    contracted_graph = Graph.new(graph.nodes)
-    graph
+def run_contraction()
+    results = []
+    n = get_graph.nodes.length
+    n = n*n
+
+    (0..n).each do
+        g = get_graph
+        graph_contraction(g)
+
+        result = [g.nodes[0].value.split("-"), g.nodes[1].value.split("-") ]
+        results.push(result)
+    end
+    # TODO return the min cut
+    results
 end
 
 
-a = Node.new("a", [])
-b = Node.new("b", [])
-c = Node.new("c", Set[a, b])
+puts run_contraction.map {|x| x.to_s}
 
-b.neighbors = Set[a, c]
-a.neighbors = Set[b, c]
-
-g = Graph.new(Set[a, b, c])
-
-
-puts a
-puts b
-puts c
-puts g
-puts choose_edge(g).map{|v| v.value.to_s}.to_s
