474402177d583846cabaa406fcbab2411f2753b5
Gabriel Milhomem <gabriel.milhomem@ul.com>
2023-04-10 16:39:16 -0300

    (Nand2Tetris I) Implementing the multiplication in asm

--- Nand2Tetris/nand2tetris/projects/04/mult/Mult.asm
+++ Nand2Tetris/nand2tetris/projects/04/mult/Mult.asm
@@ -10,3 +10,63 @@
 // R0 >= 0, R1 >= 0, and R0*R1 < 32768.
 
 // Put your code here.
+
+// Pseudocode
+
+// i = 1
+// result = 0
+
+// LOOP:
+//  if (i > R0) goto STOP
+//  result = result + R1
+//  i = i + 1
+//  goto LOOP
+// STOP:
+//  R2 = result
+
+// Actual code
+
+// i = 1
+@i
+M=1
+
+// result = 0
+
+@result
+M=0
+
+(LOOP)
+    // if (i > R0) goto STOP
+    @i
+    D=M
+    @R0
+    D=D-M
+    @STOP
+    D;JGT
+
+    // result = result + R1
+
+    @result
+    D=M
+    @R1
+    D=D+M
+    @result
+    M=D
+
+    // i = i + 1
+    @i
+    M=M+1
+
+    // goto LOOP
+
+    @LOOP
+    0;JMP
+(STOP)
+    //R2 = result
+    @result
+    D=M
+    @R2
+    M=D
+(END)
+    @END
+    0;JMP
