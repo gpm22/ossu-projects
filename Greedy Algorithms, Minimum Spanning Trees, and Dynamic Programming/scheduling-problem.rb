94b41a7867e6f2df1c075c110139167d19709bc4
Gabriel Milhomem <gabriel.milhomem@ul.com>
2023-05-05 17:49:53 -0300

    (Algorithms 3) Finishing the implementation of GreedyDiff and GreedyRation

---
+++ Greedy Algorithms, Minimum Spanning Trees, and Dynamic Programming/scheduling-problem.rb
@@ -0,0 +1,65 @@
+class Job 
+    attr_reader :lenght, :weight
+
+    def initialize(lenght, weight)
+        @lenght = lenght
+        @weight = weight
+    end
+
+    def to_s
+        "Job[lenght = #{@lenght}, weight = #{@weight}]"
+    end
+end
+
+class Schedule
+    attr_reader :jobs
+
+    def initialize(jobs)
+        @jobs = jobs
+    end
+
+
+    def getSumOfWeightedCompletionTimes
+        completionTime = 0
+        total = 0
+        @jobs.each do |job|
+            completionTime += + job.lenght
+            total += completionTime * job.weight
+        end
+        total
+    end
+
+    def greedyDiff
+        @jobs.sort!{ |job1, job2| (job2.weight-job2.lenght) <=> (job1.weight-job1.lenght) }
+        self
+    end
+
+    def greedyRatio
+        @jobs.sort!{ |job1, job2| ((job2.weight*1.0)/job2.lenght) <=> ((job1.weight*1.0)/job1.lenght) }
+        self
+    end
+
+    def to_s
+        "Schedule#{@jobs.map(&:to_s).to_s}"
+    end
+end
+
+def testSchedule
+
+    jobs = [Job.new(10, 12), Job.new(2, 4), Job.new(9, 7), Job.new(6, 7), Job.new(9, 10)]
+    schedule = Schedule.new(jobs)
+    puts "diff: #{schedule.greedyDiff.getSumOfWeightedCompletionTimes}"
+    puts "ratio: #{schedule.greedyRatio.getSumOfWeightedCompletionTimes}"
+
+end 
+
+def bookTest
+    puts "book test"
+    jobs = [Job.new(5, 3), Job.new(2, 1)]
+    schedule = Schedule.new(jobs)
+    puts "diff: #{schedule.greedyDiff.getSumOfWeightedCompletionTimes}"
+    puts "ratio: #{schedule.greedyRatio.getSumOfWeightedCompletionTimes}"
+end
+
+testSchedule
+bookTest
