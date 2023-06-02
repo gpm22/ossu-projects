5990e46e0b16172d2fdd0c6d7bc87978f0fcba05
Gabriel Milhomem <gabriel.milhomem@ul.com>
2023-06-02 19:07:14 -0300

    (Nand2Tetris II) Starting adding the getSymbol static method

--- Nand2Tetris/nand2tetris/projects/07/VMTranslator/src/CommandsTable.rb
+++ Nand2Tetris/nand2tetris/projects/07/VMTranslator/src/CommandsTable.rb
@@ -1,10 +1,10 @@
 class CommandsTable
 
-        @@arithmeticLogicalBinaryCommands = {"add" => nil, "sub" => nil,
+        @@arithmeticLogicalBinaryCommands = {"add" => "D=D+M", "sub" => "D=D-M",
                                             "eq" => nil, "gt" => nil, "lt" => nil,
-                                            "and" => nil, "or" => nil, }
+                                            "and" => "D=D&M", "or" => "D=D|M", }
 
-        @@arithmeticLogicalUnaryCommands = {"neg" => nil, "not" => nil}
+        @@arithmeticLogicalUnaryCommands = {"neg" => "D=-M", "not" => "D=!M"}
 
     def CommandsTable.isArithmeticLogicalCommand?(command)
         @@arithmeticLogicalUnaryCommands.include?(command) || @@arithmeticLogicalBinaryCommands.include?(command)
@@ -16,5 +16,11 @@ class CommandsTable
 
     def CommandsTable.isUnaryCommand?(command)
         @@arithmeticLogicalUnaryCommands.include?(command)
+    end
+
+    def CommandsTable.getSymbol(command)
+        return @@arithmeticLogicalBinaryCommands[command] if @@arithmeticLogicalBinaryCommands.include?(command)
+        return @@arithmeticLogicalUnaryCommands[command] if @@arithmeticLogicalUnaryCommands.include?(command)
+        raise "there is no symbol for command: #{command}"
     end
 end
