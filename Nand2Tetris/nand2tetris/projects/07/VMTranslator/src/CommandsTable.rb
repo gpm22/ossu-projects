62d1532c8e06e558b5f6cd63d863168c8155b268
Gabriel1000homem <gabrielpacmil@gmail.com>
2023-06-03 17:09:56 -0300

    (Nand2Tetris II) Transforming static methods in instance methods and implementing the getSymbol

--- Nand2Tetris/nand2tetris/projects/07/VMTranslator/src/CommandsTable.rb
+++ Nand2Tetris/nand2tetris/projects/07/VMTranslator/src/CommandsTable.rb
@@ -1,26 +1,49 @@
 class CommandsTable
+        
+    def initialize
+        @i = 0
+        @arithmeticLogicalBinaryCommandsNonComparison = {"add" => "D=D+M", "sub" => "D=D-M",
+                                                         "and" => "D=D&M", "or" => "D=D|M"}
 
-        @@arithmeticLogicalBinaryCommands = {"add" => "D=D+M", "sub" => "D=D-M",
-                                            "eq" => nil, "gt" => nil, "lt" => nil,
-                                            "and" => "D=D&M", "or" => "D=D|M", }
+        @arithmeticLogicalBinaryCommandsComparison = {"JEQ", "gt" => "JGT", "lt" => "JLT"}
 
-        @@arithmeticLogicalUnaryCommands = {"neg" => "D=-M", "not" => "D=!M"}
-
-    def CommandsTable.isArithmeticLogicalCommand?(command)
-        @@arithmeticLogicalUnaryCommands.include?(command) || @@arithmeticLogicalBinaryCommands.include?(command)
+        @arithmeticLogicalUnaryCommands = {"neg" => "D=-M", "not" => "D=!M"}
     end
 
-    def CommandsTable.isBinaryCommand?(command)
-        @@arithmeticLogicalBinaryCommands.include?(command)
+    def isArithmeticLogicalCommand?(command)
+         @arithmeticLogicalUnaryCommandsComparison.include?(command) || @arithmeticLogicalUnaryCommandsNonComparison.include?(command) || @arithmeticLogicalBinaryCommands.include?(command)
     end
 
-    def CommandsTable.isUnaryCommand?(command)
-        @@arithmeticLogicalUnaryCommands.include?(command)
+    def isBinaryCommand?(command)
+        @arithmeticLogicalBinaryCommands.include?(command)
     end
 
-    def CommandsTable.getSymbol(command)
-        return @@arithmeticLogicalBinaryCommands[command] if @@arithmeticLogicalBinaryCommands.include?(command)
-        return @@arithmeticLogicalUnaryCommands[command] if @@arithmeticLogicalUnaryCommands.include?(command)
+    def isUnaryCommand?(command)
+        @arithmeticLogicalUnaryCommands.include?(command)
+    end
+
+    def getSymbol(command, address)
+        return getBinaryComparisonSymbol(command, address) if @arithmeticLogicalBinaryCommandsComparison.include?(command)
+        return getBinaryNonComparisonSymbol(command, address) if @arithmeticLogicalBinaryCommandsNonComparison.include?(command)
+        return getUnarySymbol(command, address) if @arithmeticLogicalUnaryCommands.include?(command)
         raise "there is no symbol for command: #{command}"
+    end
+
+    private 
+
+    def getBinaryNonComparisonSymbol(command, address)
+        commandStr = @arithmeticLogicalBinaryCommandsNonComparison[command]
+        "#{commandStr}\n#{address}\nM=D"
+    end
+
+    def getBinaryComparisonSymbol(command, address)
+        @i += 1
+        commandStr = @arithmeticLogicalBinaryCommandsComparison[command]
+        "@TRUE.#{@i}\nD-M;#{commandStr}\n#{address}\nM=-1\n@AFTER.#{@i}\n0;JMP\n(TRUE.#{@i})\n#{address}\nM=0\n(AFTER.#{@i})"
+    end
+
+    def getUnarySymbol(command, address)
+        commandStr = @arithmeticLogicalUnaryCommands[command]
+        "#{commandStr}\n#{address}\nM=D"
     end
 end
