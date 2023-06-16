573b40991a66b21a30ae167d5ec37816b12e47dc
Gabriel Milhomem <gabriel.milhomem@ul.com>
2023-06-16 18:27:40 -0300

    (Nand2Tetris II)  Adding some starting modifications to allow a f older as input

--- Nand2Tetris/nand2tetris/projects/08/VMTranslator/VMTranslator.rb
+++ Nand2Tetris/nand2tetris/projects/08/VMTranslator/VMTranslator.rb
@@ -2,20 +2,39 @@ require_relative './src/Parser'
 require_relative './src/CodeWriter'
 
 class VMTranslator
-  def initialize(file)
-    self.setFilesInfo(file)
-    @parser = Parser.new(@file)
-    @writer = CodeWriter.new(@folder + '/' + File.basename(@file, '.vm') + '.asm')
+  def initialize(path)
+    @isFile? = !File.directory?(path)
+    if @isFile?
+      self.setFilesInfo(path)
+    else
+      self.setFoldersInfo(path)
+    end
   end
 
   def execute
-    while @parser.hasMoreLines
-        @parser.advance
-        commandType =  @parser.commandType
+    if @isFile?
+      @writer = CodeWriter.new(@parentFolder + '/' + File.basename(@file, '.vm') + '.asm')
+      self.executeFile(@file)
+    else
+      self.executeFolder
+    end
+  end
+
+  private
+
+  def executeFolder
+    raise "folders support are not implement yet"
+  end
+
+  def executeFile(fileName)
+    parser = Parser.new(fileName)
+    while parser.hasMoreLines
+        parser.advance
+        commandType =  parser.commandType
         if commandType == :C_ARITHMETIC 
-            @writer.writeArithmetic(@parser.arg1)
+            @writer.writeArithmetic(parser.arg1)
         elsif commandType == :C_POP || commandType == :C_PUSH
-            @writer.writePushPop(commandType, @parser.arg1, @parser.arg2)
+            @writer.writePushPop(commandType, parser.arg1, parser.arg2)
         else
             next
         end
@@ -23,20 +42,30 @@ class VMTranslator
     @writer.close
   end
 
-  private
+  def setFoldersInfo(folder)
+    raise "folders support are not implement yet"
+
+    @folder, @parentFolder = self.getInfoFromPath(file)
+  end
 
   def setFilesInfo(file)
+
     raise "File extension must be vm and not #{File.extname(file)}" unless File.extname(file) == '.vm'
     raise 'First file name letter must be uppercase' unless File.basename(file)[0] == File.basename(file)[0].upcase
 
-    if file.include?('\\') || file.include?('/')
-      file = file.gsub('\\', '/') if file.include?('\\')
-      @file = file
-      @folder = File.dirname(@file)
+    @file, @parentFolder = self.getInfoFromPath(file)
+  end
+
+  def getInfoFromPath(path)
+    if path.include?('\\') || path.include?('/')
+      path = path.gsub('\\', '/') if path.include?('\\')
+      newPath = path
+      parentFolder = path.dirname(newPath)
     else
-      @folder = Dir.pwd
-      @file = @folder.to_s + '/' + file.to_s
+      parentFolder = Dir.pwd
+      newPath = @parentFolder.to_s + '/' + path.to_s
     end
+    [newPath, parentFolder]
   end
 end
 
