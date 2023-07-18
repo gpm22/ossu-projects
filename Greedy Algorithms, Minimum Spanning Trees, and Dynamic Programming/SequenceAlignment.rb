c7711d41f1b3f193386aa24111e49b3a8ad90ed1
Gabriel Milhomem <gabriel.milhomem@ul.com>
2023-07-17 21:05:26 -0300

    (Algorithms 3) Implementing class Sequence Alignment

--- Greedy Algorithms, Minimum Spanning Trees, and Dynamic Programming/SequenceAlignment.rb
+++ Greedy Algorithms, Minimum Spanning Trees, and Dynamic Programming/SequenceAlignment.rb
@@ -1,42 +1,52 @@
-def needlemanWunschScore(string1, string2, gapPenalty)
+class SequenceAlignment
+  attr_reader :string1, :string2, :gapPenalty, :matchPenalties
+
+  def initialize(string1, string2, gapPenalty, matchPenalties)
+    @string1 = string1
+    @string2 = string2
+    @gapPenalty = gapPenalty
+    @matchPenalties = matchPenalties
+  end
+
+  def needlemanWunschScore
     #match penalties are received as a block
-    stringSize1 = string1.length
-    stringSize2 = string2.length
-    subproblems = Array.new(stringSize1 + 1) {Array.new(stringSize2 + 1)}
+    stringSize1 = @string1.length
+    stringSize2 = @string2.length
+    @subproblems = Array.new(stringSize1 + 1) { Array.new(stringSize2 + 1) }
     #base cases
-    (0..stringSize1).each {|i| subproblems[i][0] = i * gapPenalty}
-    (0..stringSize2).each {|i| subproblems[0][i] = i * gapPenalty}
+    (0..stringSize1).each { |i| @subproblems[i][0] = i * @gapPenalty }
+    (0..stringSize2).each { |i| @subproblems[0][i] = i * @gapPenalty }
 
     # systematically solve all subproblems
 
     (1..stringSize1).each do |i|
-        (1..stringSize2).each do |j|
-
-            case1 = subproblems[i-1][j-1] + yield(string1[i-1], string2[j-1])
-            case2 = subproblems[i-1][j] + gapPenalty
-            case3 = subproblems[i][j-1] + gapPenalty
-            subproblems[i][j] = [case1, case2, case3].min
-        end
+      (1..stringSize2).each do |j|
+        case1 = @subproblems[i - 1][j - 1] + matchPenalties.call(@string1[i - 1], @string2[j - 1])
+        case2 = @subproblems[i - 1][j] + @gapPenalty
+        case3 = @subproblems[i][j - 1] + @gapPenalty
+        @subproblems[i][j] = [case1, case2, case3].min
+      end
     end
-    subproblems[stringSize1][stringSize2]
+    @subproblems[stringSize1][stringSize2]
+  end
 end
 
 def test(string1, string2, gapPenalty, expected, testName = nil)
-    testName = "#{string1} - #{string2} - #{gapPenalty} - #{expected}" if testName.nil?
+  testName = "#{string1} - #{string2} - #{gapPenalty} - #{expected}" if testName.nil?
+  classNW = SequenceAlignment.new(string1, string2, gapPenalty, ->(a, b) { yield(a, b) })
+  result = classNW.needlemanWunschScore
 
-    result = needlemanWunschScore(string1, string2, gapPenalty) {|a, b| yield(a, b)}
+  raise "test failed!: #{testName}\nactual result: #{result} different from the expected one: #{expected}" unless result == expected
 
-    raise "test failed!: #{testName}\nactual result: #{result} different from the expected one: #{expected}" unless result == expected
-
-    puts "test passed!: #{testName}"
+  puts "test passed!: #{testName}"
 end
 
 def tests()
-    test('AGTACG','ACATAG', 1, 4, "book test") {|a, b| (a==b)? 0: 2}
-    test('ACTAGC','ACATAG', 1, 2, "book test 2") {|a, b| (a==b)? 0: 2}
-    test('ACACATGCATCATGACTATGCATGCATGACTGACTGCATGCATGCATCCATCATGCATGCATCGATGCATGCATGACCACCTGTGTGACACATGCATGCGTGTGACATGCGAGACTCACTAGCGATGCATGCATGCATGCATGCATGC',
-         'ATGATCATGCATGCATGCATCACACTGTGCATCAGAGAGAGCTCTCAGCAGACCACACACACGTGTGCAGAGAGCATGCATGCATGCATGCATGCATGGTAGCTGCATGCTATGAGCATGCAG',
-        4, 224, "website test") {|a, b| (a==b)? 0 : 5}
+  test("AGTACG", "ACATAG", 1, 4, "book test") { |a, b| (a == b) ? 0 : 2 }
+  test("ACTAGC", "ACATAG", 1, 2, "book test 2") { |a, b| (a == b) ? 0 : 2 }
+  test("ACACATGCATCATGACTATGCATGCATGACTGACTGCATGCATGCATCCATCATGCATGCATCGATGCATGCATGACCACCTGTGTGACACATGCATGCGTGTGACATGCGAGACTCACTAGCGATGCATGCATGCATGCATGCATGC",
+       "ATGATCATGCATGCATGCATCACACTGTGCATCAGAGAGAGCTCTCAGCAGACCACACACACGTGTGCAGAGAGCATGCATGCATGCATGCATGCATGGTAGCTGCATGCTATGAGCATGCAG",
+       4, 224, "website test") { |a, b| (a == b) ? 0 : 5 }
 end
 
 tests
