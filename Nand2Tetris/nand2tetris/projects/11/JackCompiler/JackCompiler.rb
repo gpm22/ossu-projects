17f98d0acaa96bdc1c898d2a1765b406c3eb325c
Gabriel1000homem <gabrielpacmil@gmail.com>
2023-08-26 12:40:28 -0300

    (Nand2Tetris II) Adapting of the JackCompiler the code to use the VMWriter

--- Nand2Tetris/nand2tetris/projects/11/JackCompiler/JackCompiler.rb
+++ Nand2Tetris/nand2tetris/projects/11/JackCompiler/JackCompiler.rb
@@ -1,69 +1,72 @@
-require_relative "./src/CompilationEngine"
-require_relative "./src/JackTokenizer"
+require_relative './src/CompilationEngine'
+require_relative './src/JackTokenizer'
+require_relative './src/VMWriter'
 
-class JackAnalyzer
+class JackCompiler
   def initialize(path)
     @isFile = !File.directory?(path)
     if @isFile
-      self.setFilesInfo(path)
+      setFilesInfo(path)
     else
-      self.setFoldersInfo(path)
+      setFoldersInfo(path)
     end
   end
 
   def execute
     if @isFile
-      self.executeFile(@file)
+      executeFile(@file)
     else
-      self.executeFolder
+      executeFolder
     end
   end
 
   private
 
-  def createEngine(tokenizer, path)
-    CompilationEngine.new(tokenizer, @folder + "/" + path + ".xml")
+  def createEngine(tokenizer, writer)
+    CompilationEngine.new(tokenizer, writer)
   end
 
   def executeFolder
     @files.each do |file|
-      executeFile(@folder + "/" + file)
+      executeFile(@folder + '/' + file)
     end
   end
 
   def executeFile(fileName)
     tokenizer = JackTokenizer.new(fileName)
-    engine = createEngine(tokenizer, File.basename(fileName, ".jack"))
+    outputPath = @folder + '/' + File.basename(fileName, '.jack') + '.vm'
+    writer = VMWriter.new(outputPath)
+    engine = createEngine(tokenizer, writer)
     engine.compileClass
     tokenizer.close
-    engine.close
+    writer.close
   end
 
   def setFoldersInfo(folder)
-    @folder = self.getInfoFromPath(folder)
-    @files = Dir.entries(@folder).select { |f| File.extname(f) == ".jack" }
+    @folder = getInfoFromPath(folder)
+    @files = Dir.entries(@folder).select { |f| File.extname(f) == '.jack' }
     raise "folder #{@folder} does not contain any jack file" if @files.size == 0
   end
 
   def setFilesInfo(file)
-    raise "File extension must be jack and not #{File.extname(file)}" unless File.extname(file) == ".jack"
-    raise "First file name letter must be uppercase" unless File.basename(file)[0] == File.basename(file)[0].upcase
+    raise "File extension must be jack and not #{File.extname(file)}" unless File.extname(file) == '.jack'
+    raise 'First file name letter must be uppercase' unless File.basename(file)[0] == File.basename(file)[0].upcase
 
-    @file = self.getInfoFromPath(file)
+    @file = getInfoFromPath(file)
   end
 
   def getInfoFromPath(path)
-    if path.include?('\\') || path.include?("/")
-      path = path.gsub('\\', "/") if path.include?('\\')
+    if path.include?('\\') || path.include?('/')
+      path = path.gsub('\\', '/') if path.include?('\\')
       newPath = path
     else
       parentFolder = Dir.pwd
-      newPath = parentFolder.to_s + "/" + path.to_s
+      newPath = parentFolder.to_s + '/' + path.to_s
     end
     newPath
   end
 end
 
 for arg in ARGV
-  JackAnalyzer.new(arg).execute
+  JackCompiler.new(arg).execute
 end
