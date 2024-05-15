d7e57bf85e9a977efb9ea1ce7bcb525e9bc35e83
Gabriel Milhomem <gabrielpacmil@gmail.com>
2024-05-15 17:40:40 -0300

    (Algorithms IV) Changing firstVertex and subproblems to be instance variables

--- Shortest Paths Revisited, NP-Complete Problems and What To Do About Them/ExactInefficientTSP/BaseGraph.rb
+++ Shortest Paths Revisited, NP-Complete Problems and What To Do About Them/ExactInefficientTSP/BaseGraph.rb
@@ -3,13 +3,13 @@ require "set"
 class BaseGraph
   #TODO version optimized
   def BellmanHeldKarp
-    subproblems = {}
+    @subproblems = {}
     # base cases (|S| = 2)
-    firstVertex = @vertices.keys[0]
+    @firstVertex = @vertices.keys[0]
     @vertices.keys.drop(1).each do |vertex|
-      setSubproblem = Set[firstVertex, vertex]
-      subproblems[setSubproblem] = {}
-      subproblems[setSubproblem][vertex] = getEdgeValue(firstVertex, vertex)
+      setSubproblem = Set[@firstVertex, vertex]
+      @subproblems[setSubproblem] = {}
+      @subproblems[setSubproblem][vertex] = getEdgeValue(@firstVertex, vertex)
     end
 
     # systematically solve all subproblems
@@ -17,16 +17,16 @@ class BaseGraph
     (3..@vertices.size).each do |subproblemSize|
       currentSubproblems = generateSubProblems(subproblemSize)
       currentSubproblems.each do |subproblem|
-        subproblems[subproblem] = {}
-        subproblemClean = subproblem - Set[firstVertex]
+        @subproblems[subproblem] = {}
+        subproblemClean = subproblem - Set[@firstVertex]
 
         subproblemClean.each do |j|
-          subproblems[subproblem][j] = getMinimal(j, subproblem, subproblems)
+          @subproblems[subproblem][j] = getMinimal(j, subproblem)
         end
       end
     end
 
-    getMinimal(@vertex[0], @vertex, subproblems)
+    getMinimal(@firstVertex, @vertices.to_set)
   end
 
   private
@@ -35,13 +35,12 @@ class BaseGraph
     [] #TODO
   end
 
-  def getMinimal(vertex, subproblem, subproblems)
+  def getMinimal(vertex, subproblem)
     subproblemClean = subproblem - Set[vertex]
-    firstVertex = @vertices.keys[0]
-    subproblemClean2 = subproblemClean - Set[firstVertex]
+    subproblemClean2 = subproblemClean - Set[@firstVertex]
     minimal = Float::INFINITY
     subproblemClean2.each do |neighbor|
-      newMinimal = subproblems[subproblemClean][neighbor] + getEdgeValue(neighbor, vertex)
+      newMinimal = @subproblems[subproblemClean][neighbor] + getEdgeValue(neighbor, vertex)
 
       minimal = newMinimal if newMinimal < minimal
     end
