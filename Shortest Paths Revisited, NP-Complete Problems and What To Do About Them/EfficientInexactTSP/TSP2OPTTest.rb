d7e98beaf56445365f380c96a6b82b2d6908bad8
Gabriel Milhomem <gabrielpacmil@gmail.com>
2024-05-17 16:50:47 -0300

    (Algorithms IV) Using generic test suit in 2opt

--- Shortest Paths Revisited, NP-Complete Problems and What To Do About Them/EfficientInexactTSP/TSP2OPTTest.rb
+++ Shortest Paths Revisited, NP-Complete Problems and What To Do About Them/EfficientInexactTSP/TSP2OPTTest.rb
@@ -1,46 +1,13 @@
-STDOUT.sync = true
-require_relative "../Graphs/Graph"
-require_relative "../Graphs/CartesianGraph"
-require_relative "./TSP2OPT"
+require_relative "../TSP_test_files/TestSuit"
 require_relative "./NearestNeighborTSP"
-require "benchmark"
-require "test/unit"
-extend Test::Unit::Assertions
+require_relative "./TSP2OPT"
 
-def getGraphFromFile(file, type)
-  parentFolder = File.expand_path("../.")
-  inputPath = parentFolder + "/TSP_test_files/#{file}.txt"
-
-  return CartesianGraph.createGraphFromFile(inputPath) if type == :CARTESIAN
-
-  Graph.createGraphFromFile(inputPath)
-end
-
-def testRandom(file, expected, name, type)
-  graph = getGraphFromFile(file, type)
-  optInstance = TSP2OPT.new(graph)
-  result = optInstance.TSP2OPT
-  puts "#{name} tour - best: #{expected} - current: #{result}"
-
-  assert_equal(expected, result[1].round(2), name)
-  puts "Test passed! #{name}"
-end
+@testSuit = TestSuit.new(Proc.new { |x| TSP2OPT.new(x).TSP2OPT[1] })
 
 def runTestFilesRandom
-  testRandom("tsptest1", 13, "quiz 19.2", nil)
-  testRandom("tsptest2", 23, "quiz 20.7", nil)
-  testRandom("tsptest3", 13.30, "", :CARTESIAN)
-end
-
-def testToVerifyPerformance(n, type)
-  puts "starting test with #{n} vertices - #{Time.now.strftime("%d/%m/%Y %H:%M")}"
-  puts "creating graph with #{n} vertices"
-  graph = type == :CARTESIAN ? CartesianGraph.generateGraphWithNVertices(n) : Graph.generateGraphWithNVertices(n)
-  optInstance = TSP2OPT.new(graph)
-  puts "running tsp -  #{Time.now.strftime("%d/%m/%Y %H:%M")}"
-  time = Benchmark.measure { optInstance.TSP2OPT }
-
-  puts "for n: #{n} - time: #{time.real}"
+  @testSuit.testFile("tsptest1", 13, "quiz 19.2", nil)
+  @testSuit.testFile("tsptest2", 23, "quiz 20.7", nil)
+  @testSuit.testFile("tsptest3", 13.30, "", :CARTESIAN)
 end
 
 def comparing2OPTWithNearestNeighbor(n, type)
@@ -62,4 +29,5 @@ def comparing2OPTWithNearestNeighbor(n, type)
   puts "2OPT    - result: #{result2OPT[1]} -  time: #{time2OPT.real}"
 end
 
+runTestFilesRandom
 10.times { comparing2OPTWithNearestNeighbor(500, :CARTESIAN) }
