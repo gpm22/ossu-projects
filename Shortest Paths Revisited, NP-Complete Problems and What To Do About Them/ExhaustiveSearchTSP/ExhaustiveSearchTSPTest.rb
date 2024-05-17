e12a269168c16565af6fc290bd051a04c6afc49e
Gabriel Milhomem <gabrielpacmil@gmail.com>
2024-05-17 16:30:03 -0300

    (Algorithms IV) Using generic test suit in exhaustive search

--- Shortest Paths Revisited, NP-Complete Problems and What To Do About Them/ExhaustiveSearchTSP/ExhaustiveSearchTSPTest.rb
+++ Shortest Paths Revisited, NP-Complete Problems and What To Do About Them/ExhaustiveSearchTSP/ExhaustiveSearchTSPTest.rb
@@ -1,49 +1,5 @@
-STDOUT.sync = true
-require_relative "../Graphs/Graph"
-require_relative "../Graphs/CartesianGraph"
+require_relative "../TSP_test_files/TestSuit"
 require_relative "./ExhaustiveSearchTSP"
-require "benchmark"
-require "test/unit"
-extend Test::Unit::Assertions
 
-def getGraphFromFile(file, type)
-  parentFolder = File.expand_path("../.")
-  inputPath = parentFolder + "/TSP_test_files/#{file}.txt"
-
-  return CartesianGraph.createGraphFromFile(inputPath) if type == :CARTESIAN
-
-  Graph.createGraphFromFile(inputPath)
-end
-
-def testFile(file, expected, name, type)
-  graph = getGraphFromFile(file, type)
-  tspInstance = ExhaustiveSearchTSP.new(graph)
-  result = tspInstance.tsp[2]
-  result = result.round(2) if type == :CARTESIAN
-  puts "#{name} tour: #{result}"
-  assert_equal(expected, result, name)
-
-  puts "Test passed! #{name}"
-end
-
-def runTestFiles
-  testFile("tsptest1", 13, "quiz 19.2", nil)
-  testFile("tsptest2", 23, "quiz 20.7", nil)
-  testFile("tsptest3", 4.90, "", :CARTESIAN)
-end
-
-def testToVerifyPerformance(n)
-  puts "starting test with #{n} vertices - #{Time.now.strftime("%d/%m/%Y %H:%M")}"
-  puts "creating graph with #{n} vertices"
-  graph = type == :CARTESIAN ? CartesianGraph.generateGraphWithNVertices(n) : Graph.generateGraphWithNVertices(n)
-  tspInstance = ExhaustiveSearchTSP.new(graph)
-  #timeNaive = Benchmark.measure { graph.tspNaive }
-  puts "running tsp"
-  timeOptimized = Benchmark.measure { tspInstance.tsp }
-
-  #puts "for n: #{n} - time naive: #{timeNaive.real} - time optimized: #{timeOptimized.real}"
-  puts "for n: #{n} - time optimized: #{timeOptimized.real}"
-end
-
-#5.times { testToVerifyPerformance(17) }
-runTestFiles
+testSuit = TestSuit.new(Proc.new { |x| ExhaustiveSearchTSP.new(x) })
+testSuit.runTestFiles
---
+++ Shortest Paths Revisited, NP-Complete Problems and What To Do About Them/TSP_test_files/TestSuit.rb
@@ -0,0 +1,50 @@
+STDOUT.sync = true
+require_relative "../Graphs/Graph"
+require_relative "../Graphs/CartesianGraph"
+require "benchmark"
+require "test/unit"
+
+class TestSuit
+  extend Test::Unit::Assertions
+
+  def initialize(tspInstanceFactory)
+    @tspInstanceFactory = tspInstanceFactory
+  end
+
+  def getGraphFromFile(file, type)
+    currentFolder = File.absolute_path(File.dirname(__FILE__))
+    inputPath = currentFolder + "/#{file}.txt"
+
+    return CartesianGraph.createGraphFromFile(inputPath) if type == :CARTESIAN
+
+    Graph.createGraphFromFile(inputPath)
+  end
+
+  def testFile(file, expected, name, type)
+    graph = getGraphFromFile(file, type)
+    tspInstance = @tspInstanceFactory.call(graph)
+    result = tspInstance.tsp[2]
+    result = result.round(2) if type == :CARTESIAN
+    puts "#{name} tour: #{result}"
+    TestSuit.assert_equal(expected, result, name)
+
+    puts "Test passed! #{name}"
+  end
+
+  def runTestFiles
+    testFile("tsptest1", 13, "quiz 19.2", nil)
+    testFile("tsptest2", 23, "quiz 20.7", nil)
+    testFile("tsptest3", 4.90, "", :CARTESIAN)
+  end
+
+  def testToVerifyPerformance(n, type)
+    puts "starting test with #{n} vertices - #{Time.now.strftime("%d/%m/%Y %H:%M")}"
+    puts "creating graph with #{n} vertices"
+    graph = type == :CARTESIAN ? CartesianGraph.generateGraphWithNVertices(n) : Graph.generateGraphWithNVertices(n)
+    tspInstance = @tspInstanceFactory.call(graph)
+    puts "running tsp"
+    time = Benchmark.measure { tspInstance.tsp }
+
+    puts "for n: #{n} - time: #{time.real}"
+  end
+end
