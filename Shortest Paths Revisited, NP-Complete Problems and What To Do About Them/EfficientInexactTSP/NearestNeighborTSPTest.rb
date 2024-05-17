3916bc239edb9f1912692f7cadf93e6bdf0c993c
Gabriel Milhomem <gabrielpacmil@gmail.com>
2024-05-17 16:46:51 -0300

    (Algorithms IV) Using generic test suit in Nearest Neighbor

--- Shortest Paths Revisited, NP-Complete Problems and What To Do About Them/EfficientInexactTSP/NearestNeighborTSPTest.rb
+++ Shortest Paths Revisited, NP-Complete Problems and What To Do About Them/EfficientInexactTSP/NearestNeighborTSPTest.rb
@@ -1,64 +1,20 @@
-STDOUT.sync = true
-require_relative "../Graphs/Graph"
-require_relative "../Graphs/CartesianGraph"
+require_relative "../TSP_test_files/TestSuit"
 require_relative "./NearestNeighborTSP"
-require "benchmark"
-require "test/unit"
-extend Test::Unit::Assertions
 
-def getGraphFromFile(file, type)
-  parentFolder = File.expand_path("../.")
-  inputPath = parentFolder + "/TSP_test_files/#{file}.txt"
-
-  return CartesianGraph.createGraphFromFile(inputPath) if type == :CARTESIAN
-
-  Graph.createGraphFromFile(inputPath)
-end
-
-def testFirst(file, expected, name, type)
-  graph = getGraphFromFile(file, type)
-  nhtInstance = NearestNeighborTSP.new(graph)
-  result = nhtInstance.nearestNeighborTSPFirst
-  puts "#{name} tour: #{result}"
-  assert_equal(expected, result[1].round(2), name)
-
-  puts "Test passed! #{name}"
-end
+@testSuit = TestSuit.new(Proc.new { |x| NearestNeighborTSP.new(x).nearestNeighborTSP[1] })
+@testSuitFirst = TestSuit.new(Proc.new { |x| NearestNeighborTSP.new(x).nearestNeighborTSPFirst[1] })
 
 def runTestFilesFirst
-  testFirst("tsptest1", 13, "quiz 19.2", nil)
-  testFirst("tsptest2", 29, "quiz 20.7", nil)
-  testFirst("tsptest3", 13.30, "", :CARTESIAN)
-end
-
-def testRandom(file, expected, name, type)
-  graph = getGraphFromFile(file, type)
-  nhtInstance = NearestNeighborTSP.new(graph)
-  result = nhtInstance.nearestNeighborTSP
-  puts "#{name} tour - best: #{expected} - current: #{result}"
-
-  assert_equal(expected, result[1].round(2), name)
-  puts "Test passed! #{name}"
+  @testSuitFirst.testFile("tsptest1", 13, "quiz 19.2", nil)
+  @testSuitFirst.testFile("tsptest2", 29, "quiz 20.7", nil)
+  @testSuitFirst.testFile("tsptest3", 13.30, "", :CARTESIAN)
 end
 
 def runTestFilesRandom
-  testRandom("tsptest1", 13, "quiz 19.2", nil)
-  testRandom("tsptest2", 24, "quiz 20.7", nil)
-  testRandom("tsptest3", 13.30, "", :CARTESIAN)
+  @testSuit.testFile("tsptest1", 13, "quiz 19.2", nil)
+  @testSuit.testFile("tsptest2", 24, "quiz 20.7", nil)
+  @testSuit.testFile("tsptest3", 13.30, "", :CARTESIAN)
 end
 
-def testToVerifyPerformance(n, type)
-  puts "starting test with #{n} vertices - #{Time.now.strftime("%d/%m/%Y %H:%M")}"
-  puts "creating graph with #{n} vertices"
-  graph = type == :CARTESIAN ? CartesianGraph.generateGraphWithNVertices(n) : Graph.generateGraphWithNVertices(n)
-  nhtInstance = NearestNeighborTSP.new(graph)
-
-  puts "running tsp -  #{Time.now.strftime("%d/%m/%Y %H:%M")}"
-  time = Benchmark.measure { nhtInstance.nearestNeighborTSPFirst }
-
-  puts "for n: #{n} - time: #{time.real}"
-end
-
-#testToVerifyPerformance(117_000, :CARTESIAN)
 runTestFilesFirst
 runTestFilesRandom
