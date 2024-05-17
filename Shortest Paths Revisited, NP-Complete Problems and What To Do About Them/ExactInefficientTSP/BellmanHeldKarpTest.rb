87b21da8d9dde787a727f6e1b0a21fb07c39830e
Gabriel Milhomem <gabrielpacmil@gmail.com>
2024-05-17 13:13:35 -0300

    (Algorithms IV) testing optimized version of the BellmanHeldKarp

--- Shortest Paths Revisited, NP-Complete Problems and What To Do About Them/ExactInefficientTSP/BellmanHeldKarpTest.rb
+++ Shortest Paths Revisited, NP-Complete Problems and What To Do About Them/ExactInefficientTSP/BellmanHeldKarpTest.rb
@@ -14,8 +14,9 @@ def getGraphFromFile(file, type)
   Graph.createGraphFromFile(inputPath)
 end
 
-def testFile(file, expected, name, type)
-  result = getGraphFromFile(file, type).BellmanHeldKarp
+def testFile(file, expected, name, type, optimized)
+  graph = getGraphFromFile(file, type)
+  result = optimized ? graph.BellmanHeldKarpOptimized : graph.BellmanHeldKarp
   result = result.round(2) if type == :CARTESIAN
   puts "#{name} tour: #{result}"
   assert_equal(expected, result, name)
@@ -24,20 +25,27 @@ def testFile(file, expected, name, type)
 end
 
 def runTestFiles
-  testFile("tsptest1", 13, "quiz 19.2", nil)
-  testFile("tsptest2", 23, "quiz 20.7", nil)
-  testFile("tsptest3", 4.90, "", :CARTESIAN)
+  testFile("tsptest1", 13, "quiz 19.2", nil, false)
+  testFile("tsptest2", 23, "quiz 20.7", nil, false)
+  testFile("tsptest3", 4.90, "", :CARTESIAN, false)
 end
 
-def testToVerifyPerformance(n, type)
+def runTestFilesOptimized
+  testFile("tsptest1", 13, "quiz 19.2", nil, true)
+  testFile("tsptest2", 23, "quiz 20.7", nil, true)
+  testFile("tsptest3", 4.90, "", :CARTESIAN, true)
+end
+
+def testToVerifyPerformance(n, type, optimized)
   puts "starting test with #{n} vertices - #{Time.now.strftime("%d/%m/%Y %H:%M")}"
   puts "creating graph with #{n} vertices"
   graph = type == :CARTESIAN ? CartesianGraph.generateGraphWithNVertices(n) : Graph.generateGraphWithNVertices(n)
   puts "running tsp -  #{Time.now.strftime("%d/%m/%Y %H:%M")}"
-  time = Benchmark.measure { graph.BellmanHeldKarp }
+  time = Benchmark.measure { optimized ? graph.BellmanHeldKarpOptimized : graph.BellmanHeldKarp }
 
   puts "for n: #{n} - time: #{time.real}"
 end
 
-#testToVerifyPerformance(117_000, :CARTESIAN)
+#testToVerifyPerformance(117_000, :CARTESIAN, true)
 runTestFiles
+runTestFilesOptimized
