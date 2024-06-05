ead38285bc9b85ecbce20b403495558c74d29c66
Gabriel Milhomem <gabrielpacmil@gmail.com>
2024-06-04 23:29:58 -0300

    (Computer Networking) Changes to run clients'

--- Computer Networking - A Top-Down Approach/chapter_2/UDPClient.py
+++ Computer Networking - A Top-Down Approach/chapter_2/UDPClient.py
@@ -1,9 +1,11 @@
 from socket import *
-serverName = 'hostname'
+serverName = ''
 serverPort = 12000
 clientSocket = socket(AF_INET, SOCK_DGRAM)
+#clientSocket.bind(('', 5432))
 message = input('Input lowercase sentence:')
 clientSocket.sendto(message.encode(),(serverName, serverPort))
 modifiedMessage, serverAddress = clientSocket.recvfrom(2048)
+print(serverAddress)
 print(modifiedMessage.decode())
 clientSocket.close()
