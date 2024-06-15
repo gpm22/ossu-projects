d1972d5db63c8dd899697f669b36e55f461d598e
Gabriel Milhomem <gabrielpacmil@gmail.com>
2024-06-15 19:03:33 -0300

    (Computer Networking) Multi-thread version of WebServer

--- Computer Networking - A Top-Down Approach/chapter_2/WebServer/WebServer.py
+++ Computer Networking - A Top-Down Approach/chapter_2/WebServer/WebServer.py
@@ -1,21 +1,16 @@
 # import socket module
 from socket import *
+import threading
 import sys  # In order to terminate the program
 
-serverSocket = socket(AF_INET, SOCK_STREAM)
-# Prepare a sever socket
-serverPort = 12000
-serverSocket.bind(("", serverPort))
-serverSocket.listen(1)
-while True:
-    # Establish the connection
-    print("Ready to serve...")
-    connectionSocket, addr = serverSocket.accept()
+
+def responseThread(connectionSocket, addr):
     try:
         message = connectionSocket.recv(1024).decode()
         print("sending to: ", addr)
         filename = message.split()[1]
         f = open(filename[1:])
+        a = input("waiting")
         outputdata = f.read()
         # Send one HTTP header line into socket
         connectionSocket.send("HTTP/1.1 200 OK\r\n".encode())
@@ -34,5 +29,24 @@ while True:
     # Close client socket
     connectionSocket.close()
 
-serverSocket.close()
-sys.exit()  # Terminate the program after sending the corresponding data
+
+def mainThread():
+    serverSocket = socket(AF_INET, SOCK_STREAM)
+    # Prepare a sever socket
+    serverPort = 12000
+    serverSocket.bind(("", serverPort))
+    serverSocket.listen(1)
+    while True:
+        # Establish the connection
+        print("Ready to serve...")
+        connectionSocket, addr = serverSocket.accept()
+        newThread = threading.Thread(
+            target=responseThread, args=(connectionSocket, addr)
+        )
+        newThread.start()
+    serverSocket.close()
+    sys.exit()  # Terminate the program after sending the corresponding data
+
+
+if __name__ == "__main__":
+    mainThread()
