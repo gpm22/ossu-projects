0d777a2b758977393b19dae4a669cdff261c8227
Gabriel Milhomem <gabrielpacmil@gmail.com>
2024-06-16 17:40:02 -0300

    (Computer Networking) Adding more stats to UDPPingerClient

--- Computer Networking - A Top-Down Approach/chapter_2/UDPPinger/UDPPingerClient.py
+++ Computer Networking - A Top-Down Approach/chapter_2/UDPPinger/UDPPingerClient.py
@@ -5,6 +5,10 @@ serverName = "localhost"
 serverPort = 12000
 clientSocket = socket(AF_INET, SOCK_DGRAM)
 clientSocket.settimeout(1)
+totalSent = 0
+min = float("inf")
+max = 0
+totalRTT = 0
 for i in range(1, 11):
     try:
         print("\nsending: ", i)
@@ -13,9 +17,26 @@ for i in range(1, 11):
         clientSocket.sendto(message.encode(), (serverName, serverPort))
         response, serverAddress = clientSocket.recvfrom(2048)
         RTT = (datetime.now() - sendingTime).total_seconds() * 1_000_000
+
+        totalSent = totalSent + 1
+        totalRTT = totalRTT + RTT
+
+        if RTT < min:
+            min = RTT
+
+        if RTT > max:
+            max = RTT
         print("response: ", response.decode())
         print("RTT: ", RTT, " µs")
     except TimeoutError:
         print("Request timed out")
 
 clientSocket.close()
+
+avg = totalRTT / totalSent
+lossRate = (10 - totalSent) / 10 * 100
+
+print(
+    "Minimum RTT: ", min, " µs - Maximum RTT: ", max, " µs - Average RTT: ", avg, " µs"
+)
+print("Packet loss rate: ", lossRate, " %")
