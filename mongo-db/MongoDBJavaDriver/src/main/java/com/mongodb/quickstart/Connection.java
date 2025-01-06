3b56eeef6074b1a293edd7481e60f8d6ec6f9874
Gabriel Milhomem <gabrielpacmil@gmail.com>
2025-01-05 23:33:52 -0300

    (Mongo) Add MongoDB connection setup and project configuration files
    This commit introduces the initial setup for a Java project connecting to MongoDB. It adds IntelliJ project configuration files, a `.gitignore` for common IDEs and environments, a Maven `pom.xml` with necessary dependencies, and a `Connection.java` class for connecting to MongoDB using dotenv for environment variables.

---
+++ mongo-db/MongoDBJavaDriver/src/main/java/com/mongodb/quickstart/Connection.java
@@ -0,0 +1,22 @@
+package com.mongodb.quickstart;
+
+import io.github.cdimascio.dotenv.Dotenv;
+import com.mongodb.client.MongoClient;
+import com.mongodb.client.MongoClients;
+import org.bson.Document;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Connection {
+
+    public static void main(String[] args) {
+        Dotenv dotenv = Dotenv.load();
+
+        String connectionString = dotenv.get("mongodb.uri");
+        try (MongoClient mongoClient = MongoClients.create(connectionString)) {
+            List<Document> databases = mongoClient.listDatabases().into(new ArrayList<>());
+            databases.forEach(db -> System.out.println(db.toJson()));
+        }
+    }
+}
